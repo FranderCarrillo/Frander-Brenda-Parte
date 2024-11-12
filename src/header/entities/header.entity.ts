283b137de7d8429f058480c2f2e2bc8c16cead32
Brenda Obando <brenobandogo0805@gmail.com>
2024-11-11 23:12:36 -0600

    changes brenda

--- src/header/entities/header.entity.ts
+++ src/header/entities/header.entity.ts
@@ -1,1 +1,20 @@
-export class Header {}
+import {
+    Column,
+    Entity,
+    PrimaryGeneratedColumn,
+  } from "typeorm";
+  
+  @Entity('header')
+  export class header {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    image: string;
+  }
